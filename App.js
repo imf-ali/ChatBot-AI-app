5efbd8a885f7a4f2cff731e1f4062e5d70853376
Fahad Ali <alifahad2210@gmail.com>
2023-04-09 02:08:33 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomePage from './src/screens/HomePage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const nagivator = createStackNavigator(
+  {
+    Home: HomePage,
   },
-});
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'Chat Bot',
+    },
+  }
+);
+
+export default createAppContainer(nagivator);
---
+++ src/screens/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function HomePage() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({});
+
+export default HomePage;
